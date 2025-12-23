40bf2570854db36e8919a77099c70f9b67b8ccaa
Mif2006 <114983743+Mif2006@users.noreply.github.com>
2025-12-24 02:41:57 +0300

    Updated AdminLayout.tsx

--- components/admin/AdminLayout.tsx
+++ components/admin/AdminLayout.tsx
@@ -26,7 +26,7 @@ export default function AdminLayout({ children }: { children: React.ReactNode })
   ];
 
   return (
-    <div className="min-h-screen bg-gray-50">
+    <div className="min-h-screen pt-7vh bg-gray-50">
       <nav className="bg-gray-900 text-white">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="flex justify-between items-center h-16">
